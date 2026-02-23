60f50523184f68fa9fd6d8048c97755211da3ec4
Fabian Gutierrez <fabiangtml@gmail.com>
2026-02-23 13:51:10 -0800

    Changing astro distribution

--- src/constants/gallery.ts
+++ src/constants/gallery.ts
@@ -1,27 +1,27 @@
 export const GALLERY_IMGS = [
     {
         cols: 2,
-        rows: 3,
+        rows: 2,
         alt: "Pam & Alex leaf",
         lightboxWidth: 800,
         lightboxHeight: 600
     },
     {
-        cols: 3,
+        cols: 2,
         rows: 4,
         alt: "Pam & Alex jump",
         lightboxWidth: 900,
         lightboxHeight: 600
     },
     {
-        cols: 2,
-        rows: 3,
+        cols: 1,
+        rows: 2,
         alt: "Pam & Alex smile",
         lightboxWidth: 900,
         lightboxHeight: 600
     },
     {
-        cols: 2,
+        cols: 1,
         rows: 2,
         alt: "Pam & Alex out",
         lightboxWidth: 900,
